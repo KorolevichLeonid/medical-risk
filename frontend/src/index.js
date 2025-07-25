74a9865f1b67a54206f1de22a3b5410abd2eddb0
KorolevichLeonid <korolevichleonid@gmail.com>
2025-07-25 20:54:13 +0300

    test3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './index.css';
 // Очистка содержимого перед рендерингом
 const container = document.getElementById('root');
 container.innerHTML = ''; // Критически важная строка
