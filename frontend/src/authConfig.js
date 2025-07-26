327f16824744251963cddc0c6042c28f810b5540
KorolevichLeonid <korolevichleonid@gmail.com>
2025-07-27 00:46:51 +0300

    Test9

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "e59f02df-4e58-4888-8d75-8a6ed15a46e4",
+    clientId: "624fdd0e-67d1-4f65-8a19-036f4c6879c6",
     authority: "https://korolevichleonid.ciamlogin.com/deb8c5e9-54cd-477d-be23-71cb103b773f",
     knownAuthorities: ["korolevichleonid.ciamlogin.com"],
     redirectUri: "https://brave-grass-08892c503.1.azurestaticapps.net/auth",
