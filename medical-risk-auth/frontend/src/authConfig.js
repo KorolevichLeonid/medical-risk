a5b2ac98ae3dc7ae59f7bba053f734fd1f108e48
KorolevichLeonid <korolevichleonid@gmail.com>
2025-11-25 06:33:48 +0300

    Finalize auth config with default scopes

--- medical-risk-auth/frontend/src/authConfig.js
+++ medical-risk-auth/frontend/src/authConfig.js
@@ -1,7 +1,8 @@
 export const msalConfig = {
   auth: {
     clientId: "624fdd0e-67d1-4f65-8a19-036f4c6879c6",
-    authority: "https://login.microsoftonline.com/deb8c5e-54cd-477d-be23-71cb103b773f",
+
+    authority: "https://login.microsoftonline.com/deb8c5e9-54cd-477d-be23-71cb103b773f",
     knownAuthorities: ["login.microsoftonline.com"],
     redirectUri: "https://medical-risk-render.onrender.com",
     postLogoutRedirectUri: "https://medical-risk-render.onrender.com"
