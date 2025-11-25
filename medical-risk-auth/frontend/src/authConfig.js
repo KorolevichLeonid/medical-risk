63f7337f5cfe358ba7b3369e7716d028004111b7
KorolevichLeonid <korolevichleonid@gmail.com>
2025-11-25 05:32:01 +0300

    Set auth redirect URI to https://medical-risk-render.onrender.com

--- medical-risk-auth/frontend/src/authConfig.js
+++ medical-risk-auth/frontend/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: "624fdd0e-67d1-4f65-8a19-036f4c6879c6",
-    authority: "https://korolevichleonid.ciamlogin.com/deb8c5e9-54cd-477d-be23-71cb103b773f/v2.0",
+    authority: "https://korolevichleonid.ciamlogin.com/deb8c5e-54cd-477d-be23-71cb103b773f/v2.0",
     knownAuthorities: ["korolevichleonid.ciamlogin.com"],
     redirectUri: "https://medical-risk-render.onrender.com",
     postLogoutRedirectUri: "https://medical-risk-render.onrender.com"
