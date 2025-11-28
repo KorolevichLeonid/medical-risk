580c4e0db88a6e73e3f1428bf225f52c5de1f634
KorolevichLeonid <korolevichleonid@gmail.com>
2025-11-28 09:34:35 +0300

    Update authConfig.js

--- medical-risk-auth/frontend/src/authConfig.js
+++ medical-risk-auth/frontend/src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "76428447-1df3-4708-b641-ac110fc0dd0b",
+    clientId: "624fdd0e-67d1-4f65-8a19-036f4c6879c6",
     authority: "https://korolevichleonid.ciamlogin.com/deb8c5e9-54cd-477d-be23-71cb103b773f/v2.0",
     knownAuthorities: ["korolevichleonid.ciamlogin.com"],
     redirectUri: "https://medical-risk-render-f.onrender.com/login",
